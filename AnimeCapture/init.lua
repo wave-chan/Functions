94dcfcc501c85cdb634750069258659b917f891e
wave <89941722+wave-chan@users.noreply.github.com>
2026-08-14 14:40:22 +0100

    Update init.lua

--- AnimeCapture/init.lua
+++ AnimeCapture/init.lua
@@ -1,3 +1,5 @@
 return {
   Gacha = loadstring(game:HttpGet("https://raw.githubusercontent.com/wave-chan/Functions/refs/heads/main/AnimeCapture/Gacha.lua"))(),
+  Progression = loadstring(game:HttpGet("https://raw.githubusercontent.com/wave-chan/Functions/refs/heads/main/AnimeCapture/Progression.lua"))(),
+  SingleUpgrades = loadstring(game:HttpGet("https://raw.githubusercontent.com/wave-chan/Functions/refs/heads/main/AnimeCapture/SingleUpgrades.lua"))(),
 }

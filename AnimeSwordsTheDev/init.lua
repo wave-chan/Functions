92f1dde6d4fc5b0d8b0ca30b326925693748491f
wave <89941722+wave-chan@users.noreply.github.com>
2026-09-02 22:43:34 +0100

    Update init.lua

--- AnimeSwordsTheDev/init.lua
+++ AnimeSwordsTheDev/init.lua
@@ -1,1 +1,4 @@
-
+return {
+  Gacha = loadstring(game:HttpGet("https://raw.githubusercontent.com/wave-chan/Functions/refs/heads/main/AnimeSwordsTheDev/Gacha.lua"))(),
+  Enemies = loadstring(game:HttpGet("https://raw.githubusercontent.com/wave-chan/Functions/refs/heads/main/AnimeSwordsTheDev/Enemies.lua"))(),
+}

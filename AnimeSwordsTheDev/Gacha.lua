42b66f317939b55e6e5cfc16cff2656c433b21bd
wave <89941722+wave-chan@users.noreply.github.com>
2026-09-02 22:52:17 +0100

    Create Gacha.lua

---
+++ AnimeSwordsTheDev/Gacha.lua
@@ -0,0 +1,52 @@
+--## SCRIPT ##--
+local module = { Cache = {}, }
+
+function module:Init(Manager, Tab, LoopCooldown)
+
+    LoopCooldown = LoopCooldown or 0.05
+    local Section = Tab:AddSection("Gachas")
+
+    local GachaArray = {}
+    for GachaName, GachaInfo in pairs(Manager.Library.Shared.Gachas) do
+        GachaInfo.Information.Index = GachaInfo.Information.Index or 1
+        table.insert(GachaArray, GachaInfo.Information.Index + #GachaArray, GachaName)
+    end
+
+    for Index, GachaName in ipairs(Manager.Library.Shared.Gachas) do
+    
+        local LoopId = `GachaRoll{GachaName}`
+        local ToggleId = `autoRollGacha_{GachaName}`
+
+        local Toggle = Section:AddToggle(ToggleId, {Title = `Auto Roll {GachaName}`, Default = false })
+
+        local Loop = Manager.Cache.Loops[LoopId]
+        if Loop or not Manager.Utils.Loop then continue end
+
+        Manager.Cache.Loops[LoopId] = Manager.Utils.Loop:Connect(LoopCooldown, function()
+            self:Roll(Manager, GachaName)
+        end, function()
+            return Toggle and Toggle.Value
+        end)
+
+    end
+end
+
+function module:Roll(Manager, GachaName: string)
+
+    --## INFO ##--
+    local GachaInfo = Manager.Library.Shared.Gachas[GachaName]
+    if not GachaInfo then return end
+
+    --## CHECKS ##--
+    local GachaCost = GachaInfo.Information.Amount
+    local CanRoll = (Manager.Library.Data.Inventory[GachaInfo.Information.Item] or 0) >= GachaCost
+
+    if not CanRoll then
+        return
+    end
+
+    Manager:Signal("General", "Gacha", "Roll", GachaName, {})
+
+end
+
+return module

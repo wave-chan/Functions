0834197dc0c259fbcbee126e3b2110a619ac0c16
wave <89941722+wave-chan@users.noreply.github.com>
2026-09-13 19:52:09 +0100

    Update Enemies module URL in init.lua

--- AnimeBreakers/init.lua
+++ AnimeBreakers/init.lua
@@ -1,4 +1,4 @@
 return {
   Gacha = loadstring(game:HttpGet("https://raw.githubusercontent.com/wave-chan/Functions/refs/heads/main/AnimeBreakers/Gacha.lua"))(),
-  --Enemies = loadstring(game:HttpGet("https://raw.githubusercontent.com/wave-chan/Functions/refs/heads/main/AnimeSwordsTheDev/Enemies.lua"))(),
+  Enemies = loadstring(game:HttpGet("https://raw.githubusercontent.com/wave-chan/Functions/refs/heads/main/AnimeBreakers/Enemies.lua"))(),
 }
